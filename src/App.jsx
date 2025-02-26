a10e7489fbf0628db6793baf3858129eba790fbf
murat-aydin01 <murataydin2001@outlook.com>
2025-02-26 15:30:08 +0300

    grid layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,34 @@ import Products from "./pages/Products";
 import Product from "./pages/Product";
 import Home from "./pages/Home";
 import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import SideBar from "./components/SideBar";
 
 function App() {
-  return <>
-    <BrowserRouter>
-    <NavBar/>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/products" element={<Products/>} />
-      <Route path="/products/:id" element={<Product/>} />
-    </Routes>
-    </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <div className="grid grid-rows-[100px_1fr_100px] grid-cols-[1fr_4fr] min-h-screen ">
+          <div className="col-span-2">
+            <NavBar />
+          </div>
+          <div>
+            <SideBar/>
+          </div>
+          <div className="p-4 border">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/products" element={<Products />} />
+              <Route path="/products/:id" element={<Product />} />
+            </Routes>
+          </div>
+          <div className="col-span-2 ">
+            <Footer />
+          </div>
+        </div>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { NavLink } from "react-router"
 
 function NavBar() {
   return (
-    <header>
+    <header className="flex justify-center gap-6">
         <NavLink to={"/"}>Ana Sayfa</NavLink>
         <NavLink to={"/products"}>Ürünler</NavLink>
     </header>
